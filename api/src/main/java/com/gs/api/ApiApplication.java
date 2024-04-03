d0a787332c5dd8cfc09ed6b3e219c902a20604e4
zhaot0392 <zhaot0392@gmail.com>
2024-04-03 17:01:23 +0800

    删除文件上传

--- api/src/main/java/com/gs/api/ApiApplication.java
+++ api/src/main/java/com/gs/api/ApiApplication.java
@@ -1,12 +1,10 @@
 package com.gs.api;
 
-import cn.xuyanwu.spring.file.storage.EnableFileStorage;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @MapperScan({"com.gs.commons.mapper"})
-@EnableFileStorage
 @SpringBootApplication(scanBasePackages = "com.gs")
 public class ApiApplication {
 
