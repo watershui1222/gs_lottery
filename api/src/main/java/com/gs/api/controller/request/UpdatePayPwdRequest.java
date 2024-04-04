91f9c17ff125eeb041d62098ebe3c7a3bf4a5c01
zhaot0392 <zhaot0392@gmail.com>
2024-04-04 19:50:46 +0800

    修改密码

--- api/src/main/java/com/gs/api/controller/request/UpdatePayPwdRequest.java
+++ api/src/main/java/com/gs/api/controller/request/UpdatePayPwdRequest.java
@@ -12,13 +12,13 @@ import javax.validation.constraints.Pattern;
 @ApiModel(value = "修改资金密码请求类", description = "修改资金密码请求参数")
 public class UpdatePayPwdRequest {
 
-    @NotNull(message = "validation.user.register.loginpwd")
-    @Pattern(regexp = RegConstant.USER_PWD_REG, message = "validation.user.register.loginpwd")
-    @ApiModelProperty(value = "旧密码", example = "123qwe", required = true)
+    @NotNull(message = "validation.user.register.paypwd")
+    @Pattern(regexp = RegConstant.WITHDRAW_PWD_REG, message = "validation.user.register.paypwd")
+    @ApiModelProperty(value = "旧密码", example = "123456", required = true)
     private String oldPwd;
 
-    @NotNull(message = "validation.user.register.loginpwd")
-    @Pattern(regexp = RegConstant.USER_PWD_REG, message = "validation.user.register.loginpwd")
-    @ApiModelProperty(value = "新密码", example = "123qwe", required = true)
+    @NotNull(message = "validation.user.register.paypwd")
+    @Pattern(regexp = RegConstant.WITHDRAW_PWD_REG, message = "validation.user.register.paypwd")
+    @ApiModelProperty(value = "新密码", example = "123456", required = true)
     private String newPwd;
 }
