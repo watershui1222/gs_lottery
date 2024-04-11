9e30834549cc5af03794260fcde4bf1b7c5b406e
lichade123321 <lichade123333@outlook.com>
2024-04-11 20:33:17 +0800

    优化BBIN拉单

--- game-record/src/main/java/com/gs/gamerecord/schedule/BBINRecordSchedule.java
+++ game-record/src/main/java/com/gs/gamerecord/schedule/BBINRecordSchedule.java
@@ -66,6 +66,7 @@ public class BBINRecordSchedule {
             //需要将时间转换成美东时间
             Date mdBegin = DateUtil.offsetHour(bbinlive.getBeginTime(), -12);
             Date mdEnd = DateUtil.offsetHour(bbinlive.getEndTime(), -12);
+            mdEnd = DateUtil.offsetMinute(mdEnd, -5);
             getLiveRecord(mdBegin, mdEnd);
             log.info("BBIN视讯---拉单完成[{}]-[{}]", DateUtil.formatDateTime(bbinlive.getBeginTime()), DateUtil.formatDateTime(bbinlive.getEndTime()));
             // 如果当前时间大于结束时间，更新拉取时间范围
@@ -94,8 +95,13 @@ public class BBINRecordSchedule {
         String strA = RandomUtil.randomString(3);
         String date = DateUtil.format(endTime, "yyyy-MM-dd");//这里的date参数必须用endTime 日期串
         //这里至少-7 -12 不然很多dateerror 因为如果使用ModifiedTime查询 BBIN不支持大于2分钟前的数据检索  而区间必须是5min
-        String endtime = DateUtil.format(DateUtil.offsetMinute(endTime, -10), "HH:mm:ss");//当action为ModifiedTime，无法捞取最近2分钟内的下注记录。
-        String starttime = DateUtil.format(DateUtil.offsetMinute(endTime, -15), "HH:mm:ss");//当action为ModifiedTime，是捞取带入的区间内被异动的纪录(区间限定5分钟且无法捞取7天前)。
+        String endtime = DateUtil.format(endTime, "HH:mm:ss");//当action为ModifiedTime，无法捞取最近2分钟内的下注记录。
+        if(StrUtil.equals("00:00:00", endtime)){
+            //无法跨天
+            endtime = "23:59:59";
+            date = DateUtil.format(DateUtil.offsetSecond(endTime, -1), "yyyy-MM-dd");
+        }
+        String starttime = DateUtil.format(DateUtil.offsetMinute(endTime, -5), "HH:mm:ss");//当action为ModifiedTime，是捞取带入的区间内被异动的纪录(区间限定5分钟且无法捞取7天前)。
         Date nowMd = DateUtil.offsetHour(DateUtil.date(), -12);
         String strB = AesUtils.MD5(website + keyB + DateUtil.format(nowMd, "yyyyMMdd"));//这里加密必须用当前美东时间
         String strC = RandomUtil.randomString(7);
@@ -108,7 +114,9 @@ public class BBINRecordSchedule {
         param.put("starttime", starttime);
         param.put("endtime", endtime);
         param.put("key", key.toLowerCase());
+        log.info("BBIN eleRecord param:{}", param);
         String result = HttpUtil.post(apiUrl, param);
+        log.info("BBIN eleRecord result:{}", result);
         JSONObject resJson = JSONObject.parseObject(result);
         JSONArray data = resJson.getJSONArray("data");
         if(resJson.getBoolean("result") && CollUtil.isNotEmpty(data)){
@@ -169,6 +177,7 @@ public class BBINRecordSchedule {
             log.info("BBIN电子---拉单开始[{}]-[{}]", DateUtil.formatDateTime(bbinele.getBeginTime()), DateUtil.formatDateTime(bbinele.getEndTime()));
             Date mdBegin = DateUtil.offsetHour(bbinele.getBeginTime(), -12);
             Date mdEnd = DateUtil.offsetHour(bbinele.getEndTime(), -12);
+            mdEnd = DateUtil.offsetMinute(mdEnd, -5);
             getEleGameRecord(mdBegin, mdEnd);
             log.info("BBIN电子---拉单完成[{}]-[{}]", DateUtil.formatDateTime(bbinele.getBeginTime()), DateUtil.formatDateTime(bbinele.getEndTime()));
             // 如果当前时间大于结束时间，更新拉取时间范围
@@ -254,8 +263,13 @@ public class BBINRecordSchedule {
         String strA = RandomUtil.randomString(3);
         String date = DateUtil.format(endTime, "yyyy-MM-dd");//这里的date参数必须用endTime 日期串
         //这里至少-7 -12 不然很多dateerror 因为如果使用ModifiedTime查询 BBIN不支持大于2分钟前的数据检索
-        String endtime = DateUtil.format(DateUtil.offsetMinute(endTime, -10), "HH:mm:ss");//当action为ModifiedTime，无法捞取最近2分钟内的下注记录。
-        String starttime = DateUtil.format(DateUtil.offsetMinute(endTime, -15), "HH:mm:ss");//当action为ModifiedTime，是捞取带入的区间内被异动的纪录(区间限定5分钟且无法捞取7天前)。
+        String endtime = DateUtil.format(endTime, "HH:mm:ss");//当action为ModifiedTime，无法捞取最近2分钟内的下注记录。
+        if(StrUtil.equals("00:00:00", endtime)){
+            //无法跨天
+            endtime = "23:59:59";
+            date = DateUtil.format(DateUtil.offsetSecond(endTime, -1), "yyyy-MM-dd");
+        }
+        String starttime = DateUtil.format(DateUtil.offsetMinute(endTime, -5), "HH:mm:ss");//当action为ModifiedTime，是捞取带入的区间内被异动的纪录(区间限定5分钟且无法捞取7天前)。
         Date nowMd = DateUtil.offsetHour(DateUtil.date(), -12);
         String strB = AesUtils.MD5(website + keyB + DateUtil.format(nowMd, "yyyyMMdd"));
         String strC = RandomUtil.randomString(7);
@@ -269,7 +283,10 @@ public class BBINRecordSchedule {
         param.put("endtime", endtime);
         param.put("subgamekind", subgamekind);
         param.put("key", key.toLowerCase());
+        log.info("BBIN eleRecord param:{}", param);
         String result = HttpUtil.post(apiUrl, param);
+        //捕鱼会出现一些{"result":false,"data":{"Code":"40014","Message":"Date Error"}}的错误,无视即可
+        log.info("BBIN eleRecord result:{}", result);
         JSONObject resJson = JSONObject.parseObject(result);
         if(resJson.getBoolean("result")){
             JSONArray data = resJson.getJSONArray("data");
@@ -295,6 +312,7 @@ public class BBINRecordSchedule {
             log.info("BBIN捕鱼---拉单开始[{}]-[{}]", DateUtil.formatDateTime(bbinfish.getBeginTime()), DateUtil.formatDateTime(bbinfish.getEndTime()));
             Date mdBegin = DateUtil.offsetHour(bbinfish.getBeginTime(), -12);
             Date mdEnd = DateUtil.offsetHour(bbinfish.getEndTime(), -12);
+            mdEnd = DateUtil.offsetMinute(mdEnd, -5);
             getFishRecord(mdBegin, mdEnd);
             log.info("BBIN捕鱼---拉单完成[{}]-[{}]", DateUtil.formatDateTime(bbinfish.getBeginTime()), DateUtil.formatDateTime(bbinfish.getEndTime()));
             // 如果当前时间大于结束时间，更新拉取时间范围
@@ -322,8 +340,13 @@ public class BBINRecordSchedule {
         String strA = RandomUtil.randomString(3);
         String date = DateUtil.format(endTime, "yyyy-MM-dd");//这里的date参数必须用endTime 日期串
         //这里至少-7 -12 不然很多dateerror 因为如果使用ModifiedTime查询 BBIN不支持大于2分钟前的数据检索
-        String endtime = DateUtil.format(DateUtil.offsetMinute(endTime, -10), "HH:mm:ss");//当action为ModifiedTime，无法捞取最近2分钟内的下注记录。
-        String starttime = DateUtil.format(DateUtil.offsetMinute(endTime, -15), "HH:mm:ss");//当action为ModifiedTime，是捞取带入的区间内被异动的纪录(区间限定5分钟且无法捞取7天前)。
+        String endtime = DateUtil.format(endTime, "HH:mm:ss");//当action为ModifiedTime，无法捞取最近2分钟内的下注记录。
+        if(StrUtil.equals("00:00:00", endtime)){
+            //无法跨天
+            endtime = "23:59:59";
+            date = DateUtil.format(DateUtil.offsetSecond(endTime, -1), "yyyy-MM-dd");
+        }
+        String starttime = DateUtil.format(DateUtil.offsetMinute(endTime, -5), "HH:mm:ss");//当action为ModifiedTime，是捞取带入的区间内被异动的纪录(区间限定5分钟且无法捞取7天前)。
         Date nowMd = DateUtil.offsetHour(DateUtil.date(), -12);
         String strB = AesUtils.MD5(website + keyB + DateUtil.format(nowMd, "yyyyMMdd"));
         String strC = RandomUtil.randomString(7);
@@ -336,7 +359,9 @@ public class BBINRecordSchedule {
         param.put("starttime", starttime);
         param.put("endtime", endtime);
         param.put("key", key.toLowerCase());
+        log.info("BBIN fishRecord param:{}", param);
         String result = HttpUtil.post(apiUrl, param);
+        log.info("BBIN fishRecord result:{}", result);
         JSONObject resJson = JSONObject.parseObject(result);
         JSONArray data = resJson.getJSONArray("data");
         if(CollUtil.isNotEmpty(data)){
