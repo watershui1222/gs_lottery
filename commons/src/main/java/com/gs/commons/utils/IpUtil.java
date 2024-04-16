ed63806c46274d5c04b91a0f9cd1e0e4f6434dd3
zhaot0392 <zhaot0392@gmail.com>
2024-04-16 16:52:21 +0800

    登录详情

--- commons/src/main/java/com/gs/commons/utils/IpUtil.java
+++ commons/src/main/java/com/gs/commons/utils/IpUtil.java
@@ -1,18 +1,16 @@
 package com.gs.commons.utils;
 
 import org.lionsoul.ip2region.xdb.Searcher;
-import org.springframework.util.ResourceUtils;
-
-import java.io.File;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 public class IpUtil {
 
     public static String getIpDetail(String ip) {
         Searcher searcher = null;
         try {
-            File file = ResourceUtils.getFile("classpath:ip2region.xdb");
-            String dbPath = file.getPath();
-            searcher = Searcher.newWithFileOnly(dbPath);
+            Resource resource = new ClassPathResource("ip2region.xdb");
+            searcher = Searcher.newWithFileOnly(resource.getFile().getPath());
             return searcher.search(ip);
         } catch (Exception e) {
             e.printStackTrace();
