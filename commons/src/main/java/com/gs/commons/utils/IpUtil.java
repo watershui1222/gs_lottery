39346c4365ec65735f519b2befd38f2c036f719f
zhaot0392 <zhaot0392@gmail.com>
2024-04-16 17:11:26 +0800

    登录详情

--- commons/src/main/java/com/gs/commons/utils/IpUtil.java
+++ commons/src/main/java/com/gs/commons/utils/IpUtil.java
@@ -1,28 +1,22 @@
 package com.gs.commons.utils;
 
+import cn.hutool.http.HttpUtil;
 import org.lionsoul.ip2region.xdb.Searcher;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 
 public class IpUtil {
 
+    private static byte[] BUFF = null;
+
+    static {
+        BUFF = HttpUtil.downloadBytes("https://juhaijituan.oss-ap-southeast-1.aliyuncs.com/gs/ip2region.xdb");
+    }
+
     public static String getIpDetail(String ip) {
-        Searcher searcher = null;
         try {
-            Resource resource = new ClassPathResource("ip2region.xdb");
-            searcher = Searcher.newWithFileOnly(resource.getFile().getPath());
+            Searcher searcher = Searcher.newWithBuffer(BUFF);
             return searcher.search(ip);
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            // 3、关闭资源
-            try {
-                if (null != searcher) {
-                    searcher.close();
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
         }
         return "未知";
     }
